e1d4970d723e8071283141174f77a91127e45681
Lipeeeeeee <favasconcelos09@gmail.com>
2024-01-17 11:23:58 -0300

    Extremamente Básico

---
+++ Extremamente-Basico/ExtremamenteBasico.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class ExtremamenteBasico{
+    public static void Main(){
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"X = {a + b}");
+    }
+}
